92c8b1834607b7d18c3591b80161b95f4059071d
이승환 <seaung13@gmail.com>
2025-01-22 11:37:12 +0900

    test: RollbackTest 추가 - 예외 종류에 따른 트랜잭션 커밋, 롤백 처리 확인

---
+++ src/test/java/com/chain/springtx/exception/RollbackTest.java
@@ -0,0 +1,72 @@
+package com.chain.springtx.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+public class RollbackTest {
+
+    @Autowired
+    RollbackService service;
+
+    @Test
+    void runtimeException() {
+        assertThatThrownBy(() -> service.runtimeException())
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void checkedException() {
+        assertThatThrownBy(() -> service.checkedException())
+                .isInstanceOf(MyException.class);
+    }
+
+    @Test
+    void rollbackFor() {
+        assertThatThrownBy(() -> service.rollbackFor())
+                .isInstanceOf(MyException.class);
+    }
+
+    @TestConfiguration
+    static class RollbackTestConfig {
+
+        @Bean
+        RollbackService rollbackService() {
+            return new RollbackService();
+        }
+    }
+
+    @Slf4j
+    static class RollbackService {
+
+        @Transactional
+        public void runtimeException() {
+            log.info("call runtimeException");
+            throw new RuntimeException();
+        }
+
+        @Transactional
+        public void checkedException() throws MyException {
+            log.info("call checkedException");
+            throw new MyException();
+        }
+
+        @Transactional(rollbackFor = MyException.class)
+        public void rollbackFor() throws MyException {
+            log.info("call rollbackFof");
+            throw new MyException();
+        }
+    }
+
+    static class MyException extends Exception {
+
+    }
+}
