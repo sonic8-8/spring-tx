cda52dfdeef7fdf4a848318bab162401809d288e
이승환 <seaung13@gmail.com>
2025-01-22 22:36:22 +0900

    feat: 트랜잭션 전파 활용 예제에 사용할 Member, MemberRepository, MemberService, Log, LogRepository 추가

---
+++ src/main/java/com/chain/springtx/propagation/Member.java
@@ -0,0 +1,27 @@
+package com.chain.springtx.propagation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    public Member() {
+
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
---
+++ src/test/java/com/chain/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,32 @@
+package com.chain.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+    @Test
+    void outerTxOff_success() {
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+}
