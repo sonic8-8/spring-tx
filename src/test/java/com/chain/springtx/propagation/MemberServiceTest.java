4e99f5fd87212a565d91ac9ca9faa1e12abad3c7
이승환 <seaung13@gmail.com>
2025-01-23 18:12:53 +0900

    test: outerTxOn_fail()를 MemberServiceTest에 추가 - 전파 롤백

--- src/test/java/com/chain/springtx/propagation/MemberServiceTest.java
+++ src/test/java/com/chain/springtx/propagation/MemberServiceTest.java
@@ -76,4 +76,15 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
     }
+
+    @Test
+    void outerTxOn_fail() {
+        String username = "로그예외_outerTxOn_fail";
+
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
 }
